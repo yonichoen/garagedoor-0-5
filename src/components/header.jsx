6bcd6c3019a29f550188375830f8b302f876c3d3
some-user <yonichoen@tutamail.com>
2024-07-16 19:02:06 -0600

    s

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -7,12 +7,39 @@ export const Header = (props) => {
         <div className="overlay">
           <div className="container">
             <div className="row">
-              <div className="col-md-8 col-md-offset-2 intro-text">
-
+              <div
+                style={{
+                  width: "100%",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  marginLeft: "0px",
+                }}
+                className="col-md-8 col-md-offset-2 intro-text"
+              >
                 <div className="text-animation">
-                  <h1 className="animated-text">              {props.data ? props.data.title : "Loading"}</h1>
+                  <h1 className="animated-text">
+                    {" "}
+                    {props.data ? props.data.title : "Loading"}
+                  </h1>
                 </div>
-                <p>{props.data ? props.data.paragraph : "Loading"}</p>
+                <p
+                  style={{
+                    fontSize: "30px",
+                    fontFamily: "sans-serif",
+                    marginBottom: "30px",
+                  }}
+                >
+                  {props.data ? props.data.paragraph : "Loading"}
+                </p>
+                <p
+                  style={{
+                    fontSize: "30px",
+                    fontFamily: "sans-serif",
+                    marginBottom: "50px",
+                  }}
+                >
+                  {props.data ? props.data.tel : "Loading"}
+                </p>
                 <a
                   href="#features"
                   className="btn btn-custom btn-lg page-scroll"
--- src/components/services.jsx
+++ src/components/services.jsx
@@ -2,23 +2,37 @@ import React from "react";
 
 export const Services = (props) => {
   return (
-    <div id="services" className="text-center" style={{backgroundColor:"#454545"}}>
-      <div className="container" style={{backgroundColor:"#454545"}}>
+    <div
+      id="services"
+      className="text-center"
+      style={{ backgroundColor: "#454545" }}
+    >
+      <div className="container" style={{ backgroundColor: "#454545" }}>
         <div className="section-title">
           <h2>Our Services</h2>
-
         </div>
         <div className="row">
           {props.data
             ? props.data.map((d, i) => (
-                <div key={`${d.name}-${i}`} className="col-md-4" style={{  display: "flex",
-                    flexDirection: "column",alignItems:"center"}}>
-                    <div className="services-image">
-                        <img src={d.img} alt="" />{" "}
-                    </div>
+                <div
+                  key={`${d.name}-${i}`}
+                  className="col-md-4"
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                  }}
+                >
+                  <div className="services-image">
+                    <img
+                      style={{ width: "110px", height: "110px" }}
+                      src={d.img}
+                      alt=""
+                    />{" "}
+                  </div>
                   <div className="service-desc">
-                    <h3>{d.name}</h3>
-                    <p>{d.text}</p>
+                    <h3 style={{ fontSize: "25px" }}>{d.name}</h3>
+                    <p style={{ fontSize: "18px" }}>{d.text}</p>
                   </div>
                 </div>
               ))
